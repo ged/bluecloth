89f4989855c70b6e853da853e558de32ba3dfc81
Michael Granger <ged@FaerieMUD.org>
2004-04-12 05:17:08 +0000

    - Converted to bug-testing testcase.
    --HG--
    extra : convert_revision : svn%3Ad6f57770-b9d7-0310-8243-b47124d2e82b/trunk%405

--- tests/10_Output.tests.rb
+++ tests/10_Output.tests.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/ruby -w
 #
-# Unit test for Markdown translation output 
+# Unit test for bugs found in BlueCloth
 # $Id: TEMPLATE.rb.tpl,v 1.2 2003/09/11 04:59:51 deveiant Exp $
 #
 # Copyright (c) 2004 The FaerieMUD Consortium.
@@ -12,11 +12,25 @@ else
 	require 'bctestcase'
 end
 
+require 'timeout'
 
 ### This test case tests ...
-class OutputTestCase < BlueCloth::TestCase
+class BugsTestCase < BlueCloth::TestCase
 
-	
+	### Test to be sure the regexen used in #hide_html_blocks isn't deathly
+	### slow.
+	def test_00_slow_block_regex
+		basedir = File::dirname( File::dirname(File::expand_path( __FILE__ )) )
+		contents = File::read( "README" )
+		bcobj = BlueCloth::new( contents )
+
+		assert_nothing_raised {
+			timeout( 2 ) do
+				bcobj.to_html
+			end
+		}
+	end
+
 
 	
 end
