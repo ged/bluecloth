cc0ee43bdfa65d8216318b5515094b59c57c88e4
Michael Granger <ged@FaerieMUD.org>
2004-04-12 05:45:07 +0000

    - Added a time() function for timing bits of code.
    --HG--
    extra : convert_revision : svn%3Ad6f57770-b9d7-0310-8243-b47124d2e82b/trunk%4011

--- utils.rb
+++ utils.rb
@@ -535,4 +535,19 @@ module UtilityFunctions
 			puts result
 		end
 	end
+
+	def time
+		start = Time::now
+		stimes = Process::times
+		rval = yield
+		etimes = Process::times
+		$stderr.puts "Time elapsed: %0.5f user, %0.5f system (%0.5f wall clock seconds)" % [
+			etimes.utime - stimes.utime,
+			etimes.stime - stimes.stime,
+			Time::now.to_f - start.to_f,
+		]
+
+		return rval
+	end
+
 end
