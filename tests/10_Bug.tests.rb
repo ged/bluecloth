06545c82f55fc36b6c5e95a590d97fa864ff231c
Michael Granger <ged@FaerieMUD.org>
2005-05-12 19:26:33 +0000

    - Added new tests for bug #17 (two-character bold/italic bug) - Added new test for bug #12 (Ruby with warnings enabled causes ArgumentError)
    --HG--
    extra : convert_revision : svn%3Ad6f57770-b9d7-0310-8243-b47124d2e82b/trunk%4073

--- tests/10_Bug.tests.rb
+++ tests/10_Bug.tests.rb
@@ -3,7 +3,7 @@
 # Unit test for bugs found in BlueCloth
 # $Id$
 #
-# Copyright (c) 2004 The FaerieMUD Consortium.
+# Copyright (c) 2004, 2005 The FaerieMUD Consortium.
 # 
 
 if !defined?( BlueCloth ) || !defined?( BlueCloth::TestCase )
@@ -49,6 +49,58 @@ class BugsTestCase < BlueCloth::TestCase
 			bcobj.to_html
 		}
 	end
+	
+	def test_20_two_character_bold_asterisks
+		html = nil
+		str = BlueCloth::new( "**aa**" )
+		assert_nothing_raised do
+			html = str.to_html
+		end
+
+		assert_equal "<p><strong>aa</strong></p>", html
+	end
+
+	def test_21_two_character_bold_underscores
+		html = nil
+		str = BlueCloth::new( "__aa__" )
+		assert_nothing_raised do
+			html = str.to_html
+		end
+
+		assert_equal "<p><strong>aa</strong></p>", html
+	end
+	
+	def test_22_two_character_emphasis_asterisks
+		html = nil
+		str = BlueCloth::new( "*aa*" )
+		assert_nothing_raised do
+			html = str.to_html
+		end
+
+		assert_equal "<p><em>aa</em></p>", html
+	end
+
+	def test_23_two_character_emphasis_underscores
+		html = nil
+		str = BlueCloth::new( "_aa_" )
+		assert_nothing_raised do
+			html = str.to_html
+		end
+
+		assert_equal "<p><em>aa</em></p>", html
+	end
+
+	def test_24_ruby_with_warnings_enabled_causes_ArgumentError
+		oldverbose = $VERBOSE
+
+		assert_nothing_raised do
+			$VERBOSE = true
+			BlueCloth.new( "*woo*" ).to_html
+		end
+	ensure
+		$VERBOSE = oldverbose
+	end
+	
 	
 end
 
