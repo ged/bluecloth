cd885be7b35cd530100e5e87312707e9cc20cbf3
Michael Granger <ged@FaerieMUD.org>
2010-01-15 20:06:53 -0800

    Add version-specific extension-loading for native gems.

--- lib/bluecloth.rb
+++ lib/bluecloth.rb
@@ -147,7 +147,16 @@ class BlueCloth
 
 end # class BlueCloth
 
-require 'bluecloth_ext'
+# Load the correct version if it's a Windows binary gem
+if RUBY_PLATFORM =~/(mswin|mingw)/i
+	major_minor = RUBY_VERSION[ /^(\d+\.\d+)/ ] or
+		raise "Oops, can't extract the major/minor version from #{RUBY_VERSION.dump}"
+	require "#{major_minor}/nokogiri"
+else
+	require 'bluecloth_ext'
+end
+
+
 
 # Set the top-level 'Markdown' constant if it isn't already set
 ::Markdown = ::BlueCloth unless defined?( ::Markdown )
