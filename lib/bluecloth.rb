c0f6486f0ad2811bea4c2d4058204c7bbc0299c0
Michael Granger <ged@FaerieMUD.org>
2011-02-09 15:38:08 -0800

    Fix for Windows devkit installs

--- lib/bluecloth.rb
+++ lib/bluecloth.rb
@@ -179,14 +179,20 @@ class BlueCloth
 
 end # class BlueCloth
 
-# Load the correct version if it's a Windows binary gem
-if RUBY_PLATFORM =~/(mswin|mingw)/i
-	major_minor = RUBY_VERSION[ /^(\d+\.\d+)/ ] or
-		raise "Oops, can't extract the major/minor version from #{RUBY_VERSION.dump}"
-	require "#{major_minor}/bluecloth_ext"
-else
+begin
 	require 'bluecloth_ext'
+rescue LoadError => err
+	# If it's a Windows binary gem, try the <major>.<minor> subdirectory
+	if RUBY_PLATFORM =~/(mswin|mingw)/i
+		major_minor = RUBY_VERSION[ /^(\d+\.\d+)/ ] or
+			raise "Oops, can't extract the major/minor version from #{RUBY_VERSION.dump}"
+		require "#{major_minor}/bluecloth_ext"
+	else
+		raise
+	end
+
 end
+
 
 
 
